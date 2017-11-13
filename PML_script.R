35d0efa476caa2159c3f6d1e34847297f88d09a2
Petr Podrouzek <petrpodrouzek@podrouzekp.local>
2017-11-13 15:47:57 +0100

    final version of the model

--- PML_script.R
+++ PML_script.R
@@ -1,46 +1,75 @@
 #Prepare the environment
+#=======================
+#include libraries required
 library(caret)
 library(rpart)
-library(rattle)		
+#library(rattle)		
 library(rpart.plot)
 library(corrplot)
-
+library(plyr)
+#set up seed and WD
 setwd("/Users/petrpodrouzek/Documents/coursera/PracticalML/QUIZ/")
-dfTrain <- read.csv("pml-training.csv", stringsAsFactors = FALSE)
-dfTest <- read.csv("pml-testing.csv")
+set.seed(1234)
 
-#take only accelerometer measurements
-#columnsToKeep <- grepl("^accel|^total_accel", names(dfTrain))
-columnsToKeep <- grepl("^accel", names(dfTrain))
+#Load datasets
+#=============
+dfTrain <- read.csv("pml-training.csv", stringsAsFactors = FALSE, na.strings=c("",NA))
+#most of the columns are numeric but contains empty strings -> needed to convert those)
+dfTrain[,8:159] <- sapply(dfTrain[,8:159],as.numeric)
+#load the data to predict on
+dfPredict <- read.csv("pml-testing.csv")
 
-#take also the last column "classe"
-columnsToKeep[length(columnsToKeep)] = TRUE
-dfTrain <- dfTrain[,columnsToKeep]
-columnsToKeep[length(columnsToKeep)] = FALSE
-dfPredict <- dfTest[,columnsToKeep]
+#Investigate the features
+#========================
+#it seems that if new_window = TRUE then aggregative measures are present
+#measures like min, max, skeweness etc.; I will firstly try to train the model
+#on the basic features for which we always have all the functions
+featureDF<-stack(sapply(dfTrain[1:160], function(y) sum(is.na(y))))
+colnames(featureDF)[1] <- "Count"
+colnames(featureDF)[2] <- "Feature"
+featureDF$ToKeep<-featureDF$Count == 0
+#the first seven columns not to be used - these are dimensions not facts
+featureDF[1:7,]$ToKeep <- FALSE
+#this is just a helper vector
+factualFeaturesToKeep<-featureDF$ToKeep
+factualFeaturesToKeep[160]<-FALSE
+#calculate correlations
+correlationMatrix <- cor(dfTrain[,factualFeaturesToKeep], use = "pairwise.complete.obs")
+#corrplot(correlationMatrix)
+print(correlationMatrix)
+#find attributes that are highly corrected (ideally >0.75)
+highlyCorrelated <- findCorrelation(na.omit(correlationMatrix), cutoff=0.75, names = TRUE)
+#print indexes of highly correlated attributes
+print(highlyCorrelated)
+corrplot(correlationMatrix)
+#remove hihgly correlated features from the feature list
+featureDF[featureDF$Feature %in% highlyCorrelated,]$ToKeep<-FALSE
+factualFeaturesToKeep<-featureDF$ToKeep
+factualFeaturesToKeep[160]<-FALSE
+correlationMatrix2 <- cor(dfTrain[,factualFeaturesToKeep], use = "pairwise.complete.obs")
+print(correlationMatrix2)
+corrplot(correlationMatrix2)
 
+#Model training and prediction
+#==============================
+#divide into training and test datasets
 inTrain<-createDataPartition(y=dfTrain$classe,p=0.75, list=FALSE)
 dfTest<-dfTrain[-inTrain,]
 dfTrain<-dfTrain[inTrain,]
 
-set.seed(1234)
-
-modelCART <- rpart(classe ~ ., data = dfTrain)
-predictCART <- predict(modelCART, dfTest, type="class")
+#decision tree - low accuracy, not working; accuracy is only 0.64
+modelCART <- rpart(classe ~ ., data = dfTrain[,featureDF$ToKeep])
+predictCART <- predict(modelCART, dfTest[,featureDF$ToKeep], type="class")
 confusionMatrix(dfTest$classe,predictCART)
 
+#random forrest - better as accuracy is about 0.98
+modelRF <- train(classe ~ ., data = dfTrain[,featureDF$ToKeep], method = "rf", verbose = FALSE, ntree = 10)
+predictRF <- predict(modelRF, dfTest[,featureDF$ToKeep], type="raw")
+confusionMatrix(dfTest$classe,predictRF)
 
-#testing on correlation
-correlationMatrix <- cor(dfTrain[,1:12])
-corrplot(correlationMatrix)
-#"classe"n# summarize the correlation matrix
-print(correlationMatrix)
-# find attributes that are highly corrected (ideally >0.75)
-highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
-# print indexes of highly correlated attributes
-print(highlyCorrelated)
+#review importance of various features
+importance <- varImp(modelRF, scale=FALSE)
+print(importance)
+plot(importance)
 
-modelSVM <- train(classe ~ ., data = dfTrain, method = "svm", verbose = FALSE)
-modelRF <- train(classe ~ ., data = dfTrain, method = "rf", verbose = FALSE)
-modelGBM <- train(classe ~ ., data = dfTrain, method = "gbm", verbose = FALSE)
-modelLDA <- train(classe ~ ., data = dfTrain, method = "lda", verbose = FALSE)
+
